f3d0f2e2a21f710c78c130fc4124104e2644a2e8
alapena <angel.lapena.lopez@gmail.com>
2026-02-17 12:30:51 +0100

    Added CUDA support to diffuse.py

--- diffuse.py
+++ diffuse.py
@@ -3,6 +3,7 @@ from src.utils import get_path, set_device, find_closest_power_of_2
 from pathlib import Path
 from tqdm import tqdm
 import numpy as np
+import torch
 import yaml
 
 def main():
@@ -17,7 +18,7 @@ def main():
     _, n_qubits = find_closest_power_of_2(n_pixels, return_power=True)
 
     dir, filename = get_path(config, type='initialqstates', n_data=n_data, n_pixels=n_pixels, n_qubits=n_qubits) # Editable
-    dataset = np.load(dir / filename)
+    dataset = torch.from_numpy(np.load(dir / filename), device=device)
 
     # Initialize the model
     n_timesteps, n_data = config['model']['n_timesteps'], dataset.shape[0]
@@ -25,16 +26,19 @@ def main():
 
     # Get the diffused states
     max_diffusion_weight = model_config.get('max_diffusion_weight', 4.0)
-    diffusion_weights = np.linspace(1., max_diffusion_weight, n_timesteps) # Hyperparameter that controls the 'size' of diffusion steps.
+    # diffusion_weights = np.linspace(1., max_diffusion_weight, n_timesteps) # Hyperparameter that controls the 'size' of diffusion steps.
+    diffusion_weights = torch.linspace(1., max_diffusion_weight, n_timesteps) # Hyperparameter that controls the 'size' of diffusion steps.
 
-    states = np.zeros((n_timesteps+1, n_data, 2**n_qubits), dtype=np.complex64)
+    # states = np.zeros((n_timesteps+1, n_data, 2**n_qubits), dtype=np.complex64)
+    states = torch.zeros((n_timesteps+1, n_data, 2**n_qubits),device=device, dtype=torch.complex64)
     states[0] = dataset
     for t in tqdm(range(1, n_timesteps+1)):
         states[t] = model.set_diffusionData_t(t, states[0], diffusion_weights[:t], seed=t)
-        states[t] = states[t] / np.linalg.norm(states[t], axis=1, keepdims=True) # Avoid numerical errors
+        # states[t] = states[t] / np.linalg.norm(states[t], axis=1, keepdims=True) # Avoid numerical errors
+        states[t] = states[t] / torch.norm(states[t], dim=1, keepdim=True) # Avoid numerical errors
 
     dir, filename = get_path(config, type='diffusedqstates', n_data=n_data, n_pixels=n_pixels, n_qubits=n_qubits, n_timesteps=n_timesteps)
-    np.save(dir / filename, states)
+    np.save(dir / filename, states.detach().numpy())
 
     print(f"Saved diffused quantum stated in {dir / filename}")
 
